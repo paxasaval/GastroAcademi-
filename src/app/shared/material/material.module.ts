ffc48f8bdff1546ab8e4bc79a65cc1299879dd18
paxasaval <paxasaval1003@gmail.com>
2021-12-29 09:21:14 -0500

    btn-menu|maquetacion  y routerlink

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 
@@ -12,12 +15,16 @@ import {MatCardModule} from '@angular/material/card';
     CommonModule,
     MatButtonModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   exports: [
     MatButtonModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
